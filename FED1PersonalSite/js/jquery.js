cc2de0a1da9b3b5aa77ab3234e04a159137fb04f
William McCarty <kcchief@yahoo.com>
2021-08-11 22:04:23 -0500

    Updated the light box for image resonsive for mobile

--- FED1PersonalSite/js/jquery.js
+++ FED1PersonalSite/js/jquery.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function () {
 $(function () {
     $(".thumb").on("click", function () {
         var imgSrc = $(this).attr("src");
-        $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive" />');
+        $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive-lightBox" />');
         $("#lightbox-container").fadeIn(1300);
         $("#lightbox-container").on("click", function () {
             $(this).fadeOut(500);
